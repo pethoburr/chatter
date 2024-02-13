32c765f6e7df9190b6d38ddd1f03a6f82d64b786
Maninder Pahal <mpahal123@gmail.com>
2024-02-12 19:07:58 -0800

    completed login component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,50 @@
 import '../App.css';
+import { ChangeEvent, useState } from 'react';
 
 
 const Home = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsername = (e: ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value)
+    }
+
+    const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        fetch('http://localhost:3000', {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({username, password})
+    }).then((resp) => {
+        resp.json()
+        console.log(resp)
+    })
+    }
+
+    
+
     return(
         <>
             <div>dis da home page gang</div>
+            <form onSubmit={(e) => { handleSubmit(e)}}>
+                <h1>Log In</h1>
+                <label>Username:
+                    <input type='text' onChange={(e) => handleUsername(e)} />
+                </label>
+                <label>Password:
+                    <input type='text' onChange={(e) => handlePassword(e)} />
+                </label>
+                <button type='submit'>Log In</button>
+            </form>
         </>
     )
 }
