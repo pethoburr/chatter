41a76fcce5c6dd15518d43286cc83a122ea1f0bd
Maninder Pahal <mpahal123@gmail.com>
2024-06-01 18:11:11 -0700

    display form if new invite recieved

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -37,6 +37,13 @@ const Home = () => {
     const navigator = useNavigate()
     const [newMsg, setNewMsg] = useState(false)
     const [firsty, setFirsty] = useState(false)
+    const [newInv, setNewInv] = useState(false)
+    const [invId, setInvId] = useState(0)
+    const [invTitle, setInvTitle] = useState(false)
+    const [invPpl, setInvPpl] = useState<string[]>([])
+    const [invited, setInvited] = useState([])
+    const [yesCheck, setYesCheck] = useState(false)
+    const [noCheck, setNoCheck] = useState(false)
 
     interface Message {
         content: string,
@@ -196,6 +203,16 @@ const Home = () => {
         getUsername(e.target.value)
     }
 
+    const handleYesCheck = () => {
+        setNoCheck(false)
+        setYesCheck(true)
+    }
+
+    const handleNoCheck = () => {
+        setNoCheck(true)
+        setYesCheck(false)
+    }
+
     const removeGuy = (guy: Members) => {
         const arr = addedMembers.filter(dude => dude.name !== guy.name)
         setAddedMembers(arr)
@@ -288,19 +305,38 @@ const Home = () => {
         }
     }
 
+    useEffect(() => {
+        if (socket !== null) {
+            socket.on('invite', (id, title, people) => {
+                setNewInv(true)
+                setInvId(id)
+                setInvTitle(title)
+                setInvited(people)
+            })
+        }
+    })
+
     const invite = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         console.log(`user: ${userId}, added in invite: ${JSON.stringify(addedMembers)}`)
         if (socket !== null) {
             chats.forEach((chat) => {
                 if (chat.id === currentRoom) {
-                    addedMembers.forEach((id) => {
-                        socket.emit('join-room', chat.title, id, chat.id)
+                    addedMembers.forEach(async (id) => {
+                        const resp = await fetch(`https://localhost:3000/get-name/${id}`)
+                        const jayed: string = await resp.json()
+                        invPpl.push(jayed)
                     })
+                    socket.emit('invite', chat.id, chat.title, invPpl)
                 }
             })
+            
         }
         closeInviteModal()
+    }
+
+    const handleJoiner = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
     }
 
     useEffect(() => {
@@ -403,46 +439,64 @@ const Home = () => {
                 </div>
             </div>
             </div>
+            { newInv && (
+            <form onSubmit={(e) => handleJoiner(e)}>
+                <p>Join Group? {invTitle}</p>
+                <p>Group members:
+                    <ul>
+                        {invited && invited.map((guy) => {
+                            return(
+                                <li>{guy}</li>
+                            )
+                            
+                        })}
+                    </ul>
+                </p>
+                <input type='checkbox' onChange={handleYesCheck}>Yes</input>
+                <input type='checkbox' onChange={handleNoCheck}>No</input>
+                <button type='submit'>Submit</button>
+            </form>
+            )}
             </div>
             <div className="msgNform">
             <div className="msgContainer">
            { currentRoom && (
             <>
                 <button onClick={() => leaveGroup()}>Leave Group</button>
-<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#inviteModal">
-  Invite
-</button>
-<div className="modal fade" id="inviteModal" tabIndex={-1} aria-labelledby="inviteModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="inviteModalLabel">Select user</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-      <form onSubmit={(e) => invite(e)}>
-                        <div className="form-group">
-                            <label htmlFor="recipient-name" className="col-form-label">Recipients:</label>
-                            <select value={memberId} onChange={(e) => handleMembers(e)}>
-                            { users && users.map((user) => {
-                            return(
-                                <option key={user.id} value={user.id} >{user.username}</option>
-                            )
-                            })}
-                        </select>
-                        { addedMembers && addedMembers.map((guy) => {
-                            return <div>{guy.name}<button onClick={() => removeGuy(guy)}>-</button></div>
-                        })}
-                        </div>
-                        <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" className="btn btn-primary">Add</button>
-                    </div>
-                        </form>
-      </div>
-    </div>
-  </div>
-</div>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#inviteModal">
+            Invite
+            </button>
+            <div className="modal fade" id="inviteModal" tabIndex={-1} aria-labelledby="inviteModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="inviteModalLabel">Select user</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                <form onSubmit={(e) => invite(e)}>
+                                    <div className="form-group">
+                                        <label htmlFor="recipient-name" className="col-form-label">Recipients:</label>
+                                        <select value={memberId} onChange={(e) => handleMembers(e)}>
+                                        { users && users.map((user) => {
+                                        return(
+                                            <option key={user.id} value={user.id} >{user.username}</option>
+                                        )
+                                        })}
+                                    </select>
+                                    { addedMembers && addedMembers.map((guy) => {
+                                        return <div>{guy.name}<button onClick={() => removeGuy(guy)}>-</button></div>
+                                    })}
+                                    </div>
+                                    <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" className="btn btn-primary">Add</button>
+                                </div>
+                                    </form>
+                </div>
+                </div>
+            </div>
+            </div>
             </>)} 
                 {messages.length > 0 ? 
                     messages.map((oj) => (
