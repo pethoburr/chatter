d164495118aff1fe10f99415dd0fbcb2df435e99
Maninder Pahal <mpahal123@gmail.com>
2024-06-25 20:29:51 -0700

    display users first, last and user names

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,42 @@
-import { useEffect } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { UserContext } from '../App';
+
 import Nav from './Nav'
 
 const Profile = () => {
     const { userId } = useParams()
+    const { token } = useContext(UserContext)
+    const [userObj, setUserObj] = useState({
+        first_name: '',
+        last_name: '',
+        username: ''
+    })
 
     useEffect(() => {
         console.log(`user id in profile: ${userId}`)
-    })
+
+        const fetchData = async() => {
+            const data = await fetch(`http://localhost:3000/profile/${userId}`, { headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`}})
+            const jayed = await data.json()
+            console.log('jayed data:' + JSON.stringify(jayed))
+            const { first_name, last_name, username } = jayed
+            setUserObj({ first_name, last_name, username})
+        }
+        fetchData()
+    },[])
+    
     return(
         <>
-            <Nav />
-            <p>Profile page</p>
+            <div className="profileContianer">
+                <Nav />
+                <div className='profilePage'>
+                    <div>First name: {userObj.first_name}</div>
+                    <div>Last name: {userObj.last_name}</div>
+                    <div>Username: {userObj.username}</div>
+                </div>
+
+            </div>
         </>
     )
 }
